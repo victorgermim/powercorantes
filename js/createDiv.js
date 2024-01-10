11855012f0b14617720a91c5d00f68048abc1505
Victor Germim Prado <152669348+victorgermim@users.noreply.github.com>
2024-01-10 18:35:21 -0300

    Add files via upload

---
+++ js/createDiv.js
@@ -0,0 +1,28 @@
+// script.js
+function criarDivs(quantidade, cor, nome) {
+    const coresContainer = document.getElementById('cores');
+
+    for (let i = 0; i < quantidade; i++) {
+        const divPrincipal = document.createElement('div');
+        divPrincipal.classList.add('cor-element');
+
+        const squareContainer = document.createElement('div');
+        squareContainer.classList.add('square-container');
+        squareContainer.style.backgroundColor = cor;
+
+        const textContainer = document.createElement('div');
+        textContainer.classList.add('text-container');
+        const texto = document.createElement('p');
+        texto.textContent = nome;
+        textContainer.appendChild(texto);
+
+        divPrincipal.appendChild(squareContainer);
+        divPrincipal.appendChild(textContainer);
+
+        coresContainer.appendChild(divPrincipal);
+    }
+}
+
+// Exemplo de uso
+criarDivs(3, 'red', 'Vermelho');
+criarDivs(3, 'blue', 'Azul');
